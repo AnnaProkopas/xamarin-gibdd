5e8486c79e98011692b66c550fc104e51c00f18a
AnnaProkopas <anna_prokopas@mail.ru>
2020-06-09 11:07:46 +1000

    timer

--- Gibdd/Gibdd/MainPage.xaml.cs
+++ Gibdd/Gibdd/MainPage.xaml.cs
@@ -14,10 +14,60 @@ namespace Gibdd
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
-        public const string LabelSampleText = "CONST TEXT";
+        public AutoIncrementalValueProvider timer;
         public MainPage()
         {
             InitializeComponent();
+
+            timer = new AutoIncrementalValueProvider();
+
+            var binding = new Binding()
+            {
+                Source = timer,
+                Path = "Counter",
+            };
+
+            timerLabel.SetBinding(Label.TextProperty, binding);
+        }
+    }
+    public class AutoIncrementalValueProvider : INotifyPropertyChanged
+    {
+        private int counter;
+        private int fastCounter;
+        public int Counter {
+            get => counter; 
+            private set {
+                counter = value;
+                OnPropertyChanged("Counter");
+            }
+        }
+        public int FastCounter
+        {
+            get => fastCounter;
+            private set
+            {
+                fastCounter = value;
+                OnPropertyChanged("Counter");
+            }
+        }
+        public AutoIncrementalValueProvider()
+        {
+            Counter = 300;
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                Counter += 1;
+                return true;
+            });
+            Device.StartTimer(TimeSpan.FromSeconds(300), () =>
+            {
+                Counter += 1;
+                return true;
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string property) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
         }
     }
 }
